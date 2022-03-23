19dc925987f957601d1d64ee1fddc31d999cd4da
김응철 <zbqlr456@naver.com>
2022-03-23 23:40:20 +0900

    퇴사

---
+++ Algorithm/Main_14501_퇴사.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_14501_퇴사 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+
+
+    }
+}
