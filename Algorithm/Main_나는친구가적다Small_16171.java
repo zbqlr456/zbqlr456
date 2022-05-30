c48ea36e63c527b71f5cfc4133681ae914a7f086
zbqlr456 <zbqlr456@naver.com>
2022-05-30 23:02:19 +0900

    그룹단어체커

--- Algorithm/Main_나는친구가적다Small_16171.java
+++ Algorithm/Main_나는친구가적다Small_16171.java
@@ -1,19 +1,22 @@
 import java.util.*;
 import java.io.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main_나는친구가적다Small_16171 {
     public static void main(String[] args) throws IOException {
-        // 알파벳 소문자, 대문자, 숫자로 이루어진 문자열 S
-        // 찾고자하는 알파벳 소문자, 대문자로만 이루어진 키워드 문자열 K
-        // 정규표현식으로 숫자를 제외한 문자열 찾음
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str1 = br.readLine();
         String str2 = br.readLine();
+        String str3 = str1.replaceAll("[0-9]", "");
 
-        Pattern pattern = Pattern.compile("^[a-zA-Z]*$");
-        Matcher matcher = pattern.matcher(str1);
+//        boolean result = str3.contains(str2);
+
+        boolean result = false;
+        for(int i=0;i<=str3.length() - str2.length();i++){
+            if(str3.substring(i,i+str2.length()).equals(str2))
+                result = true;
+        }
+
+        System.out.println(result ? 1 : 0);
 
     }
 }
