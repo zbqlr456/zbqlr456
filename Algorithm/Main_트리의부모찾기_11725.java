2b8ed8959ec0799e564d9e382f9ad14f7551fe96
zbqlr456 <zbqlr456@naver.com>
2022-06-16 21:31:53 +0900

    0616김응철

---
+++ Algorithm/Main_트리의부모찾기_11725.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_트리의부모찾기_11725 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+        for(int n=0;n<N;n++){
+
+        }
+    }
+}
