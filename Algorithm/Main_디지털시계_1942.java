e578adcada4c9403da25a1ab6b5f11eabe4c811a
zbqlr456@naver.com <zbqlr456@naver.com>
2022-06-29 20:44:16 +0900

    단어정렬

---
+++ Algorithm/Main_디지털시계_1942.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_디지털시계_1942 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
--- Algorithm/Main_행성연결_16398.java
+++ Algorithm/Main_행성연결_16398.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main_행성연결_16398 {
     static int[] parents;
-    static int minCost = 0;
+    static Planet[] planets;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
@@ -19,8 +19,30 @@ public class Main_행성연결_16398 {
         } // input
 
         parents = makeSet(N);
+        // 간선 갯수 = (N*(N-1)) / 2
 
+        planets = new Planet[(N*(N-1)) / 2];
 
+        int pNum = 0;
+        for (int i=1;i<N;i++){
+            for(int j=i+1;j<N+1;j++) {
+                planets[pNum] = new Planet(i, j, map[i][j]);
+                pNum++;
+            }
+        }
+
+        Arrays.sort(planets);
+
+        int cnt=0;
+        long result=0;
+        for(Planet planet : planets){
+            if(union(planet.start,planet.end)){
+                result += planet.cost;
+                if(++cnt == N-1) break;
+            }
+        }
+
+        System.out.println(result);
 
     }
 
@@ -41,12 +63,30 @@ public class Main_행성연결_16398 {
         }
     }
 
-    private static void union(int a, int b){
+    private static boolean union(int a, int b){
         int aRoot = find(a);
         int bRoot = find(b);
 
-        if(aRoot != bRoot){
-            parents[bRoot] = aRoot;
+        if(aRoot == bRoot)
+            return false;
+
+        parents[bRoot] = aRoot;
+        return true;
+    }
+
+    public static class Planet implements Comparable<Planet>{
+        int start,end,cost;
+
+        public Planet(int start, int end, int cost){
+            super();
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Planet o) {
+            return Integer.compare(this.cost, o.cost);
         }
     }
 }
