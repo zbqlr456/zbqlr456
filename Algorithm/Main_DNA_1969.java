6c2673c0c6f66bdcde74c4d00e720a6444f17780
zbqlr456@naver.com <zbqlr456@naver.com>
2022-07-17 20:50:28 +0900

    DNA 문제 잘못 해석한거 수정

--- Algorithm/Main_DNA_1969.java
+++ Algorithm/Main_DNA_1969.java
@@ -4,28 +4,64 @@ import java.io.*;
 public class Main_DNA_1969 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
 
-        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         char[][] DNA = new char[N][M];
-        int[][] hd = new int[N][N];
+        int hd = 0;
 
         for(int i=0;i<N;i++){
-            String tmp = br.readLine();
-            DNA[i] = tmp.toCharArray();
+            DNA[i] = br.readLine().toCharArray();
         }
 
-        int cnt = 0;
-        for(int n=0;n<N;n++){
-            for(int i=n+1;i<N;i++){
-                for(int j=0;j<M;j++){
-
+        for(int i=0;i<M;i++){
+            int[] nCnt = new int[4]; // T G C A
+            for(int j=0;j<N;j++){
+                if(DNA[j][i] == 'T'){
+                    nCnt[0]++;
+                }else if(DNA[j][i] == 'G'){
+                    nCnt[1]++;
+                }else if(DNA[j][i] == 'C'){
+                    nCnt[2]++;
+                }else if(DNA[j][i] == 'A'){
+                    nCnt[3]++;
                 }
             }
+            int max = -1;
+            int maxIndex = 0;
+            for(int j=0;j<4;j++){
+                if(nCnt[j] >= max){
+                    max = nCnt[j];
+                    maxIndex = j;
+                }
+            }
+            char maxN = 'X';
+            if(maxIndex == 0){
+                sb.append('T');
+                maxN = 'T';
+            }else if(maxIndex == 1){
+                sb.append('G');
+                maxN = 'G';
+            }else if(maxIndex == 2){
+                sb.append('C');
+                maxN = 'C';
+            }else {
+                sb.append('A');
+                maxN = 'A';
+            }
+
+            for(int j=0;j<N;j++){
+                if(DNA[j][i] != maxN)
+                    hd++;
+            }
+
         }
+
+        System.out.println(sb);
+        System.out.println(hd);
 
     }
 }
