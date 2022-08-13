077d15a1865ac05b301101ee4fbb8ebbbbace718
zbqlr456 <zbqlr456@naver.com>
2022-08-13 13:51:05 +0900

    포켓몬:

--- Algorithm/포켓몬.py
+++ Algorithm/포켓몬.py
@@ -1,3 +1,13 @@
 def solution(nums):
     answer = 0
+
+    if len(set(nums)) > len(nums)//2:
+        answer = len(nums)//2
+    else:
+        answer = len(set(nums))
+
     return answer
+
+print(solution([3,1,2,3]))
+print(solution([3,3,3,2,2,4]))
+print(solution([3,3,3,2,2,2]))
